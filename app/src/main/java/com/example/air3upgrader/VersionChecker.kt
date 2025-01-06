18fda0ddfd5d28bf0b2de110e9bcee85734ab820
Berfon99 <bertrand@lesfontaines.be>
2025-01-06 09:55:17 +0100

    ca ne crashe plus avec APK download et refresh et versionning solved

--- app/src/main/java/com/example/air3upgrader/VersionChecker.kt
+++ app/src/main/java/com/example/air3upgrader/VersionChecker.kt
@@ -43,27 +43,40 @@ class VersionChecker {
         return appsData.apps.find { it.`package` == packageName }
     }
 
-    fun isServerVersionHigher(installedVersion: String?, serverVersion: String?, packageName: String): Boolean {
+    fun isServerVersionHigher(
+        installedVersion: String?,
+        serverVersion: String?,
+        packageName: String
+    ): Boolean {
         if (installedVersion == null || serverVersion == null) {
             return false
         }
+
         val installedParts = when (packageName) {
             "org.xcontest.XCTrack" -> installedVersion.split(".", "-")
             "indysoft.xc_guide" -> listOf(installedVersion)
             else -> listOf(installedVersion)
         }
-        val serverParts = serverVersion.split(".")
+        val serverParts = serverVersion.split(".", "-")
 
         val maxLength = maxOf(installedParts.size, serverParts.size)
 
         for (i in 0 until maxLength) {
-            val installedPart = installedParts.getOrElse(i) { "0" }.toIntOrNull() ?: 0
-            val serverPart = serverParts.getOrElse(i) { "0" }.toIntOrNull() ?: 0
+            val installedPart = installedParts.getOrElse(i) { "0" }
+            val serverPart = serverParts.getOrElse(i) { "0" }
 
-            if (serverPart > installedPart) {
+            val installedNum = installedPart.toIntOrNull() ?: 0
+            val serverNum = serverPart.toIntOrNull() ?: 0
+
+            if (serverNum > installedNum) {
                 return true
-            } else if (serverPart < installedPart) {
+            } else if (serverNum < installedNum) {
                 return false
+            }
+            if (installedPart.contains("-") && !serverPart.contains("-")) {
+                return false
+            } else if (!installedPart.contains("-") && serverPart.contains("-")) {
+                return true
             }
         }
         return false
