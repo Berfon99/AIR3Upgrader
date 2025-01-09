547038ff1efc44540f23517c314b98b257b35d01
Berfon99 <bertrand@lesfontaines.be>
2025-01-09 16:31:47 +0100

    Meilleure solution jusqu'ici 2

--- app/src/main/java/com/example/air3upgrader/VersionComparator.kt
+++ app/src/main/java/com/example/air3upgrader/VersionComparator.kt
@@ -1,30 +1,46 @@
 package com.example.air3upgrader
 
-import android.util.Log
-
 object VersionComparator {
     fun isServerVersionHigher(installedVersion: String, serverVersion: String, packageName: String): Boolean {
         if (installedVersion == "N/A" || serverVersion == "N/A") {
             return false
         }
-        return try {
-            val installedVersionParts = installedVersion.split(".").map { it.toInt() }
-            val serverVersionParts = serverVersion.split(".").map { it.toInt() }
 
-            val maxLength = maxOf(installedVersionParts.size, serverVersionParts.size)
-            for (i in 0 until maxLength) {
-                val installed = installedVersionParts.getOrElse(i) { 0 }
-                val server = serverVersionParts.getOrElse(i) { 0 }
-                if (server > installed) {
-                    return true
-                } else if (server < installed) {
-                    return false
-                }
+        val installedParts = splitVersionString(installedVersion, packageName)
+        val serverParts = splitVersionString(serverVersion, packageName)
+
+        val maxParts = maxOf(installedParts.size, serverParts.size)
+
+        for (i in 0 until maxParts) {
+            val installedPart = installedParts.getOrElse(i) { "" }
+            val serverPart = serverParts.getOrElse(i) { "" }
+
+            val comparisonResult = compareVersionParts(installedPart, serverPart)
+            if (comparisonResult != 0) {
+                return comparisonResult > 0
             }
-            false // Versions are equal
-        } catch (e: NumberFormatException) {
-            Log.e("VersionComparator", "Error parsing version for $packageName: $e")
-            false // Assume not higher if parsing fails
+        }
+
+        return false
+    }
+
+    private fun splitVersionString(version: String, packageName: String): List<String> {
+        return when (packageName) {
+            "org.xcontest.XCTrack" -> version.split(".", "-")
+            "indysoft.xc_guide" -> listOf(version)
+            "com.xc.r3" -> version.split(".")
+            else -> version.split(".", "-")
+        }
+    }
+
+    private fun compareVersionParts(installedPart: String, serverPart: String): Int {
+        val installedNum = installedPart.toIntOrNull()
+        val serverNum = serverPart.toIntOrNull()
+
+        if (installedNum != null && serverNum != null) {
+            return serverNum.compareTo(installedNum)
+        } else {
+            return serverPart.compareTo(installedPart)
         }
     }
 }
