2679fdacfad67eb9fc9f818017a5d24943f5e9ee
Berfon99 <bertrand@lesfontaines.be>
2025-01-09 16:12:54 +0100

    ok et on avance 2

--- app/src/main/java/com/example/air3upgrader/AppUtils.kt
+++ app/src/main/java/com/example/air3upgrader/AppUtils.kt
@@ -9,17 +9,37 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
 object AppUtils {
-    fun getAppVersion(context: Context, packageName: String): String? {
+    fun getAppVersion(context: Context, packageName: String): String {
         return try {
             val packageInfo = context.packageManager.getPackageInfo(packageName, 0)
-            packageInfo.versionName
+            val fullVersionName = packageInfo.versionName
+
+            if (fullVersionName != null) {
+                when (packageName) {
+                    "org.xcontest.XCTrack" -> {
+                        // Remove everything after the first hyphen
+                        fullVersionName.substringBefore("-")
+                    }
+                    "indysoft.xc_guide" -> {
+                        // Remove everything before the last dot
+                        fullVersionName.substringAfterLast(".")
+                    }
+                    "com.xc.r3" -> {
+                        // Remove everything after the last dot
+                        fullVersionName.substringBeforeLast(".")
+                    }
+                    else -> fullVersionName // Return the full version for other apps
+                }
+            } else {
+                "N/A" // Handle the case where versionName is null
+            }
         } catch (e: PackageManager.NameNotFoundException) {
-            null
+            "N/A"
         }
     }
 
     suspend fun setAppBackgroundColor(context: Context, packageName: String, nameTextView: TextView, installedVersion: String?, selectedModel: String?) {
-        val serverVersion = getServerVersion(packageName, selectedModel ?: "")
+        val serverVersion = getServerVersion(packageName, selectedModel)
         if (serverVersion != null && installedVersion != null) {
             if (VersionComparator.isServerVersionHigher(installedVersion, serverVersion, packageName)) {
                 nameTextView.setBackgroundColor(ContextCompat.getColor(context, R.color.update_available))
@@ -31,8 +51,8 @@ object AppUtils {
         }
     }
 
-    private suspend fun getServerVersion(packageName: String, selectedModel: String): String? = withContext(Dispatchers.IO) {
-        val appInfos = VersionChecker().getLatestVersionFromServer(selectedModel)
+    private suspend fun getServerVersion(packageName: String, selectedModel: String?): String? = withContext(Dispatchers.IO) {
+        val appInfos = VersionChecker().getLatestVersionFromServer(selectedModel ?: "")
         return@withContext appInfos.find { it.packageName == packageName }?.latestVersion
     }
 }
