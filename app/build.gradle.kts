a308a86ea3e001f653f5f528eb88410ae44f77e6
Berfon99 <bertrand@lesfontaines.be>
2025-01-21 21:46:46 +0100

    Genial ++++ avec Minsdk27.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.air3upgrader"
-        minSdk = 33
+        minSdk = 27
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
